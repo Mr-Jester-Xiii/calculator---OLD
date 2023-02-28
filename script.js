8be80cfcfaa5f8a7efbd4977031a5d585032fb7a
Mr-Jester-Xiii <jestersilvester13@gmail.com>
2023-02-28 14:17:19 +0000

    Fixed bug on multiply function, and added clear function to button

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const numButtons = document.querySelectorAll('.btn-number');
-const opButtons = document.querySelectorAll('.btn-operator');
-const display = document.querySelector('.display');
-const equalsButton = document.querySelector('.btn-equals');
-
+const numButtons = document.querySelectorAll(".btn-number");
+const opButtons = document.querySelectorAll(".btn-operator");
+const display = document.querySelector(".display");
+const equalsButton = document.querySelector(".btn-equals");
+const clearButton = document.getElementById("clear");
 
 let displayData = "";
 let operatorPressed = false;
@@ -14,83 +14,94 @@ let result = 0;
 
 // Working event listener to populate display with button values
 
-
-numButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        let buttonValue = button.getAttribute('data-num');
-        displayData += buttonValue;
-        display.textContent = displayData;
-        if (operatorPressed === true) {
-            num2 = displayData;
-        } else {
-            num1 = displayData;
-        }
-        console.log("num1 is " + num1 + " " + "num2 is " + num2);
-    })
-})
-
-opButtons.forEach(opButton => {
-    opButton.addEventListener('click', () => {
-        let buttonValue = opButton.getAttribute('data-num');
-        operator = buttonValue;
-        displayData += buttonValue;
-        display.textContent = displayData;
-        operatorPressed = true;
-        displayData = "";
-        console.log(operatorPressed);
-    })
-})
-
-
-equalsButton.addEventListener('click', () => {
-    num1 = parseFloat(num1);
-    num2 = parseFloat(num2);
-    displayData = operate(operator, num1, num2);
+numButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    let buttonValue = button.getAttribute("data-num");
+    displayData += buttonValue;
     display.textContent = displayData;
-    console.log(num1, operator, num2);
-} )
+    if (operatorPressed === true) {
+      num2 = displayData;
+    } else {
+      num1 = displayData;
+    }
+    console.log("num1 is " + num1 + " " + "num2 is " + num2);
+  });
+});
+
+opButtons.forEach((opButton) => {
+  opButton.addEventListener("click", () => {
+    let buttonValue = opButton.getAttribute("data-num");
+    operator = buttonValue;
+    displayData += buttonValue;
+    display.textContent = displayData;
+    operatorPressed = true;
+    displayData = "";
+    console.log(operatorPressed);
+  });
+});
+
+equalsButton.addEventListener("click", () => {
+  num1 = parseFloat(num1);
+  num2 = parseFloat(num2);
+  displayData = operate(operator, num1, num2);
+  display.textContent = displayData;
+  console.log(num1, operator, num2);
+});
+
+clearButton.addEventListener("click", () => {
+    clearCalc();
+    display.textContent = displayData;
+});
 
 function addNum(num1, num2) {
-    result = (num1 + num2);
-    return result;
+  result = num1 + num2;
+  return result;
 }
 
-function subtractNum(num1, num2){
-    result = (num1 - num2);
-    return result;
+function subtractNum(num1, num2) {
+  result = num1 - num2;
+  return result;
 }
 
-function multiplnum2Num(num1, num2){
-    result = (num1 * num2);
-    return result;
+function multiplyNum(num1, num2) {
+  result = num1 * num2;
+  return result;
 }
 
-function divideNum(num1, num2){
-    result = (num1 / num2);
-    return result;
+function divideNum(num1, num2) {
+  result = num1 / num2;
+  return result;
 }
 
-function operate(operator, num1, num2){
-    switch(operator) {
-        case '+':
-            addNum(num1, num2);
-            break;
+function operate(operator, num1, num2) {
+  switch (operator) {
+    case "+":
+      addNum(num1, num2);
+      break;
 
-        case '-':
-            subtractNum(num1, num2);
-            break;
+    case "-":
+      subtractNum(num1, num2);
+      break;
 
-        case '*':
-            multiplyNum(num1, num2);
-            break
+    case "*":
+      multiplyNum(num1, num2);
+      break;
 
-        case '/':
-            divideNum(num1, num2);
-            break
-    }
-    return result;
+    case "/":
+      divideNum(num1, num2);
+      break;
+  }
+  return result;
+}
 
-    
+function clearCalc() {
+  displayData = "";
+  operatorPressed = false;
+  equalsPressed = false;
+  num1 = 0;
+  num2 = 0;
+  operator = "";
+  result = 0;
 }
 
 // function test() {
